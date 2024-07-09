77c54bc561603f1cb855ff7c4edcad6f063f7540
KaKeeM <bella.juju08@gmail.com>
2024-07-09 08:57:50 -0300

    Adicionado a lista ao formato dinamico de titulo e cor

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,14 @@ import Layout from './components/Layout';
 import {ThemeProvider} from 'styled-components';
 import lightTheme from './styles/themes/dark';
 import Dashboard from './pages/Dashboard';
+import List from './pages/List';
 
 const App: React.FC  = () => {
     return (
         <ThemeProvider theme={lightTheme}>
             <GlobalStyles/>
             <Layout>
-                <Dashboard/>
+                <List/>
             </Layout>
         </ThemeProvider>
     );
--- src/Pages/List/Index.tsx
+++ src/Pages/List/Index.tsx
@@ -1,9 +1,22 @@
 import React from "react";
+import {Container} from './styles';
+import ContentHeader from "../../components/ContentHeader";
+import SelectInput from "../../components/SelectInput";
 
 //FC é functional componente : => componente funcional
 const list: React.FC  = () => {
+    const options = [
+        { value: 'Julia', label: 'Julia' },
+        { value: 'Maria', label: 'Maria' },
+        { value: 'Ana', label: 'Ana' }
+    ];
+
     return (
-        <h1>list</h1>
+        <Container>
+            <ContentHeader title="Entradas" lineColor="#e44c4e">
+                <SelectInput options={options} />
+            </ContentHeader>
+        </Container>
 
     )
 }
