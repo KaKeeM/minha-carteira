25b81812dbc46fe529836ef8ff6c4947027f1de2
KaKeeM <bella.juju08@gmail.com>
2024-07-23 22:11:36 -0300

    filtragem

--- src/Pages/List/Index.tsx
+++ src/Pages/List/Index.tsx
@@ -6,6 +6,8 @@ import SelectInput from "../../components/SelectInput";
 import HistoryFinanceCard from "../../components/HistoryFinanceCard";
 import expenses from "../../repositories/expenses";
 import gains from "../../repositories/gains";
+import formatCurrency from "../../util/fromatCurrency";
+import formatDate from "../../util/formatDate";
 
 interface IData {
     id: string,
@@ -20,7 +22,8 @@ interface IData {
 const List: React.FC = () => {
 
     const[data, setData] = useState<IData[]>([]);
-
+    const [monthSelected, setMonthSelected] = useState<string>(String(new Date().getMonth() + 1));
+    const [yearSelected, setYearSelected] = useState<string>(String(new Date().getFullYear() + 1));
     const { type } = useParams<Record<string, string>>();
 
     const title = useMemo(() => {
@@ -40,26 +43,41 @@ const List: React.FC = () => {
 
     //toda vez que a tela é carregada
     useEffect(() => {
-        const response = listData.map(item => {
+        const filteredDate = listData.filter(item => {
+            const date = new Date(item.date);
+            const day = String(date.getDate());
+            const month = String(date.getMonth() + 1);
+            const year = String(date.getFullYear());
+
+            return month === monthSelected && year === yearSelected;
+        });
+
+        const formattedData = filteredDate.map(item => {
             return {
-                id: String(Math.random () * data.length),
+                id: String(new Date().getTime()) + item.amount,
                 description: item.description,
-                amountFormatted: item.amount,
+                amountFormatted: formatCurrency(Number(item.amount)),
                 frequency: item.frequency,
-                dataFormatted: item.date,
+                dataFormatted: formatDate(item.date),
                 tagColor: item.frequency === 'recorrente' ? '#4e41f0' : '#e44c4e'
             }
-        })
-        setData(response);
-    },[]);
+        });
+
+        setData(formattedData);
+    },[listData, monthSelected, yearSelected,data.length]);
 
     const months = [
+        { value: 5, label: 'Maio'},
         { value: 7, label: 'Julho' },
         { value: 8, label: 'Agosto'},
-        { value: 9, label: 'Setembro' }
+        { value: 9, label: 'Setembro' },
+        { value: 10, label: 'Outubro'},
     ];
 
     const years = [
+        { value: 2024, label: 2024 },
+        { value: 2023, label: 2023 },
+        { value: 2022, label: 2022 },
         { value: 2021, label: 2021 },
         { value: 2020, label: 2020 },
         { value: 2019, label: 2019 },
@@ -68,8 +86,8 @@ const List: React.FC = () => {
     return (
         <Container>
             <ContentHeader title={title.title} lineColor={title.lineColor}>
-                <SelectInput options={months} />
-                <SelectInput options={years} />
+                <SelectInput options={months} onChange={(e) => setMonthSelected(e.target.value)} defaultValue={monthSelected}/>
+                <SelectInput options={years} onChange={(e) => setYearSelected(e.target.value)} defaultValue={yearSelected}/>
             </ContentHeader>
 
             <Filters>
